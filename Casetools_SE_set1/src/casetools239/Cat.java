ae5fa20fdb7ce147e7f806099e9d25b7d069d0f0
exam <exam@Bcalab4-m25>
2024-04-29 12:26:18 +0530

    commit

---
+++ Casetools_SE_set1/src/casetools239/Cat.java
@@ -0,0 +1,17 @@
+package casetools239;
+
+public class Cat {
+	private String name;
+	private String colour;
+	public Cat(String name, String breed)
+	{
+		this.name= name;
+		this.colour=colour;
+	}
+	
+	public String getColour()
+	{
+		return colour;
+	}
+
+}
